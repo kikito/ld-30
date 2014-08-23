5bae69418597dbc5d6d865ce3141c2661f3bd6e6
kikito <kikito@gmail.com>
2014-08-23 16:36:21 +0200

    balls and players are now entities

--- src/level.lua
+++ src/level.lua
@@ -1,36 +1,12 @@
 local class = require 'lib.middleclass'
 local media = require 'media'
 
-local Level = class('Level')
+
+local World = class('World')
 
 local TILE_WIDTH, TILE_HEIGHT = 32, 32
 
-local function getRegionRect(region)
-  local w,h = love.graphics.getDimensions()
-
-  if region == 'up' then
-    return 0,0,w,h/2
-  else
-    return 0,h/2,w,h/2
-  end
-end
-
-local function getWorldRect(world, region)
-
-  local rl,rt,rw,rh = getRegionRect(region)
-
-
-  local ww, wh = world.width, world.height -- in tiles
-
-  ww, wh = ww * TILE_WIDTH, wh * TILE_HEIGHT
-
-  return rl + rw/2 - ww/2,
-         rt + rh/2 - wh/2,
-         ww,
-         wh
-end
-
-local charTranslation = {
+local charQuadTranslation = {
   up = {
     ['@'] = 'human',
     ['.'] = 'ground',
@@ -50,34 +26,96 @@ local charTranslation = {
 }
 
 
-local function getQuad(char, region)
-  local quadName = charTranslation[region][char]
+local function getQuadFromChar(char, region)
+  local quadName = charQuadTranslation[region][char]
   return media.quads[quadName]
 end
 
-local function drawWorld(world, region)
-  local l,t,w,h = getWorldRect(world, region)
+local function getRegionRect(region)
+  local w,h = love.graphics.getDimensions()
 
-  love.graphics.setColor(255,255,255)
-  for x = 1, world.width do
-    for y = 1, world.height do
-      local quad = getQuad(world.cells[y][x], region)
-      love.graphics.draw(media.img.atlas,
-                         quad,
-                         l + (x - 1) * TILE_WIDTH,
-                         t + (y - 1) * TILE_HEIGHT)
+  if region == 'up' then
+    return 0,0,w,h/2
+  else
+    return 0,h/2,w,h/2
+  end
+end
+
+local function getWorldRect(world, region)
+
+  local rl,rt,rw,rh = getRegionRect(region)
+
+  local ww, wh = world.width, world.height -- in tiles
+
+  ww, wh = ww * TILE_WIDTH, wh * TILE_HEIGHT
+
+  return rl + rw/2 - ww/2,
+         rt + rh/2 - wh/2,
+         ww,
+         wh
+end
+
+local function getCellLT(world_l, world_t, x, y)
+  return world_l + (x - 1) * TILE_WIDTH,
+         world_t + (y - 1) * TILE_HEIGHT
+end
+
+function World:initialize(world_data, region)
+  self.region = region
+  self.width = world_data.width
+  self.height = world_data.height
+
+  self.cells = {}
+  self.balls = {}
+
+  for y=1, self.height do
+    self.cells[y] = {}
+    for x=1, self.width do
+      local char = world_data.cells[y][x]
+      if     char == '@' then
+        if self.player then error('Two players defined on the same world') end
+        self.player = {x=x,y=y}
+        char = '.'
+      elseif char == 'O' then
+        self.balls[#self.balls + 1] = {x=x,y=y}
+        char = '.'
+      end
+      self.cells[y][x] = char
     end
   end
 end
 
+function World:draw(l,t)
+  love.graphics.setColor(255,255,255)
+  for x = 1, self.width do
+    for y = 1, self.height do
+      local quad = getQuadFromChar(self.cells[y][x], self.region)
+      love.graphics.draw(media.img.atlas, quad, getCellLT(l,t,x,y))
+    end
+  end
+
+  local ball_quad = getQuadFromChar('O', self.region)
+
+  for i=1,#self.balls do
+    local ball = self.balls[i]
+    love.graphics.draw(media.img.atlas, ball_quad, getCellLT(l,t, ball.x, ball.y))
+  end
+
+  local player_quad = getQuadFromChar('@', self.region)
+  love.graphics.draw(media.img.atlas, player_quad, getCellLT(l,t,self.player.x, self.player.y))
+end
+
+
+local Level = class('Level')
+
 function Level:initialize(map)
-  self.up = map.up
-  self.down = map.down
+  self.up = World:new(map.up, 'up')
+  self.down = World:new(map.down, 'down')
 end
 
 function Level:draw()
-  drawWorld(self.up, 'up')
-  drawWorld(self.down, 'down')
+  self.up:draw(getWorldRect(self.up, 'up'))
+  self.down:draw(getWorldRect(self.down, 'down'))
 end
 
 return Level
